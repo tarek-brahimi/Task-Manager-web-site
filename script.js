f90a75a3304874234e62a732eda8632072b58bea
tarek brahimi <tarekbrahimi98@gmail.com>
2026-02-15 01:54:37 +0100

    update1

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const toDolist = [];
-const toDolist2 = ["hi"];
-
+const toDolist2 = [];
+const toDate =[];
 renderTodolist();
 function renderTodolist() {
   let todohtml = "";
   for (let i= 0; i< toDolist2.length; i++) {
     const todo = toDolist2[i];
-    const html = `<p> ${todo}</p>`;
+    const date = toDate[i];
+    const html = `<p> ${todo}  ${date}<button onclick="toDolist2.splice(${i},1);renderTodolist()"> delete</button>  </p> `;
     todohtml += html;
+    
   }
   document.getElementById("the-list").innerHTML= todohtml;
 }
@@ -21,9 +23,14 @@ function addList() {
 }
 function addList2() {
   let input2 = document.getElementById("todoElement2");
+  let input3 =document.getElementById('date');
   const the_things2 = input2.value;
+  const the_date=input3.value;
+  toDate.push(the_date);
   toDolist2.push(the_things2);
   console.log(toDolist2);
+  console.log(toDate);
   input2.value = "";
+  input3.value="";
   renderTodolist();
 }
