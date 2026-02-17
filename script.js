4ad00243be5e14f6cf3babe5eb9f9419ce548206
tarek brahimi <tarekbrahimi98@gmail.com>
2026-02-17 15:56:35 +0100

    switch to for each methode

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const toDolist = [];
-const toDolist2 = [{name: 'make dinner', dueDate: '2022-12-22'}];
+const toDolist2 = [{ name: "make dinner", dueDate: "2022-12-22" }];
 
 renderTodolist();
 function renderTodolist() {
   let todohtml = "";
-  for (let i= 0; i< toDolist2.length; i++) {
-    const todo = toDolist2[i];
+  toDolist2.forEach(function (todo, index) {
     const name2 = todo.name;
     const date2 = todo.dueDate;
-    const html = `<div>${name2}</div>  <div>${date2} </div>  <button onclick="toDolist2.splice(${i},1);renderTodolist()" class="red-button"> delete</button> `;
+    const html = `<div>${name2}</div>  <div>${date2} </div>  <button onclick="toDolist2.splice(${index},1);renderTodolist()" class="red-button"> delete</button> `;
     todohtml += html;
-  
-    
+  });
+
+  document.querySelector(".grid").innerHTML = todohtml;
 }
- document.querySelector('.grid').innerHTML= todohtml;}
 
 function addList() {
   let input = document.getElementById("todoElement");
@@ -23,19 +22,18 @@ function addList() {
   input.value = "";
 }
 function addList2() {
-const inputElement = document.getElementById('todoElement2');
+  const inputElement = document.getElementById("todoElement2");
   const name = inputElement.value;
 
-  const dateInputElement = document.getElementById('date');
+  const dateInputElement = document.getElementById("date");
   const dueDate = dateInputElement.value;
 
-  
   toDolist2.push({
     name,
     dueDate,
   });
- 
- inputElement.value = "";
- 
+
+  inputElement.value = "";
+
   renderTodolist();
 }
